0222f496598dc20bf7869d6b0320d17299aeebcb
thenaveensharma <naveenchaurasiya1501@gmail.com>
2025-01-09 02:09:09 +0530

    improve folder structure and implement basic engine functionality

--- src/main.rs
+++ src/main.rs
@@ -1,113 +1,24 @@
-use std::collections::HashMap;
-
-#[derive(Debug)]
-enum BidOrAsk {
-    Bid,
-    Ask,
-}
-
-#[derive(Debug)]
-struct OrderBook {
-    asks: HashMap<Price, Limit>,
-    bids: HashMap<Price, Limit>,
-}
-
-impl OrderBook {
-    fn new() -> OrderBook {
-        OrderBook {
-            asks: HashMap::new(),
-            bids: HashMap::new(),
-        }
-    }
-    fn add_order(&mut self, price: f64, order: Order) {
-        match order.bid_or_ask {
-            BidOrAsk::Bid => {
-                let price = Price::new(price);
-                match (self.bids.get_mut(&price)) {
-                    Some(limit) => {
-                        limit.add_order(order);
-                    }
-                    None => {
-                        let mut limit = Limit::new(price);
-                        limit.add_order(order);
-                        self.bids.insert(price, limit);
-                    }
-                }
-            }
-            BidOrAsk::Ask => {
-                let price = Price::new(price);
-                match (self.asks.get_mut(&price)) {
-                    Some(limit) => {
-                        limit.add_order(order);
-                        print!("{:?}",order)
-                    }
-                    None => {
-                        let mut limit = Limit::new(price);
-                        limit.add_order(order);
-                        self.asks.insert(price, limit);
-                    }
-                }
-            }
-        }
-    }
-}
-
-#[derive(Debug, PartialEq, Eq, Hash, Clone, Copy)]
-struct Price {
-    integral: u64,
-    fractional: u64,
-    scalar: u64,
-}
-
-impl Price {
-    fn new(price: f64) -> Price {
-        let scalar = 100_000;
-        let integral = price as u64;
-        let fractional = ((price % 1.0) * scalar as f64) as u64;
-        Price {
-            scalar,
-            integral,
-            fractional,
-        }
-    }
-}
-#[derive(Debug)]
-struct Limit {
-    price: Price,
-    orders: Vec<Order>,
-}
-
-impl Limit {
-    fn new(price: Price) -> Limit {
-        Limit {
-            price,
-            orders: Vec::new(),
-        }
-    }
-    fn add_order(&mut self, order: Order) {
-        self.orders.push(order);
-    }
-}
-
-#[derive(Debug)]
-struct Order {
-    size: f64,
-    bid_or_ask: BidOrAsk,
-}
-
-impl Order {
-    fn new(bid_or_ask: BidOrAsk, size: f64) -> Order {
-        Order { bid_or_ask, size }
-    }
-}
-
+mod matching_engine;
+use matching_engine::engine::{MatchingEngine, TradingPair};
+use matching_engine::orderbook::{BidOrAsk, Order, OrderBook};
 fn main() {
+    // let buy_order_from_naveen = Order::new(BidOrAsk::Ask, 16.0);
+
+    // let mut orderbook = OrderBook::new();
+
+    // orderbook.add_order(199.012, buy_order_from_naveen);
+    // orderbook.add_order(199.012, buy_order_from_nik);
+    // // println!("{:?}", orderbook);
+
+    let mut engine = MatchingEngine::new();
+    let base = String::from("BTC");
+    let quote = String::from("USDT");
+    let pair = TradingPair::new(base, quote);
+    let price = 100.00;
     let buy_order_from_nik = Order::new(BidOrAsk::Ask, 141.0);
-    let buy_order_from_naveen = Order::new(BidOrAsk::Ask, 16.0);
+    engine.add_market(pair.clone());
 
-    let mut orderbook = OrderBook::new();
-
-    orderbook.add_order(199.012, buy_order_from_naveen);
-    orderbook.add_order(199.012, buy_order_from_nik);
-    println!("{:?}", orderbook);
+    engine
+        .place_limit_order(pair, price, buy_order_from_nik)
+        .unwrap()
 }

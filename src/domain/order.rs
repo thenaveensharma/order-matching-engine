cf7362338904668a3920bc1359f02f4f61d99da9
thenaveensharma <naveenchaurasiya1501@gmail.com>
2025-01-24 00:23:30 +0530

    add order schema

--- src/domain/order.rs
+++ src/domain/order.rs
@@ -1,18 +1,35 @@
-use crate::db::schema::orders;
+use crate::matching_engine::types::decimal::Price;
 use chrono::NaiveDateTime;
-use rust_decimal::Decimal;
+use diesel::prelude::Queryable;
 use serde::{Deserialize, Serialize};
+use tokio_postgres::Row;
 
-#[derive(Debug, Queryable, Insertable, Serialize, Deserialize, AsChangeset)]
-#[table_name = "orders"]
+#[derive(Debug, Serialize, Deserialize, Queryable)]
+#[diesel(table_name = orders)]
 pub struct Order {
     pub id: i32,
     pub user_id: uuid::Uuid,
     pub token_id: i32,
     pub order_type: String,
-    pub price: Decimal,
-    pub amount: Decimal,
+    pub price: Price,
+    pub amount: Price,
     pub status: String,
     pub created_at: NaiveDateTime,
     pub updated_at: NaiveDateTime,
+}
+
+impl Order {
+    pub fn from_row(row: &Row) -> Self {
+        Order {
+            id: row.get("id"),
+            user_id: row.get("user_id"),
+            token_id: row.get("token_id"),
+            order_type: row.get("order_type"),
+            price: row.get("price"),
+            amount: row.get("amount"),
+            status: row.get("status"),
+            created_at: row.get("created_at"),
+            updated_at: row.get("updated_at"),
+        }
+    }
 }
